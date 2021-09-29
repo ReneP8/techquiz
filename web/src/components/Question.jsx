774db43aaee6c34122e3feebce6a67192008dbef
ReneP8 <Rene-Baving@web.de>
2021-09-29 22:41:06 +0200

    designed random question page

--- web/src/components/Question.jsx
+++ web/src/components/Question.jsx
@@ -1,19 +1,38 @@
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Row, Col } from "react-bootstrap";
 
 function Question(props) {
   return (
-    <Card style={{ width: "18rem" }}>
+    <Card>
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text>
-          {props.question}
-        </Card.Text>
-        <Button variant="primary">{props.correctAnswer}</Button>
-        <Button variant="primary">{props.incorrectAnswers[0]}</Button>
-        <Button variant="primary">{props.incorrectAnswers[1]}</Button>
-        <Button variant="primary">{props.incorrectAnswers[2]}</Button>
+        <Card.Text>{props.question}</Card.Text>
+        <Row className="mt-3">
+          <Col className="d-grid gap-2">
+            <Button variant="secondary" bsSize="large" block>
+              {props.correctAnswer}
+            </Button>
+          </Col>
+          <Col className="d-grid gap-2">
+            <Button variant="secondary" bsSize="large" block>
+              {props.incorrectAnswers[0]}
+            </Button>
+          </Col>
+        </Row>
+        <Row className="mt-3">
+          <Col className="d-grid gap-2">
+            <Button variant="secondary" bsSize="large" block>
+              {props.incorrectAnswers[1]}
+            </Button>
+          </Col>
+          <Col className="d-grid gap-2">
+            <Button variant="secondary" bsSize="large" block>
+              {props.incorrectAnswers[2]}
+            </Button>
+          </Col>
+        </Row>
       </Card.Body>
-    </Card>);
+    </Card>
+  );
 }
 
 export default Question;
