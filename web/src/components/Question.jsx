7c18002c2117375d5a6eb18efe67491396ca0e38
ReneP8 <Rene-Baving@web.de>
2021-11-01 14:08:29 +0100

    unescraped special characters

--- web/src/components/Question.jsx
+++ web/src/components/Question.jsx
@@ -7,6 +7,12 @@ function Question(props) {
     array.sort(() => Math.random() - 0.5);
   }
 
+  function htmlDecode(input) {
+    var doc = new DOMParser().parseFromString(input, "text/html");
+    return doc.documentElement.textContent;
+  }
+  
+
   function prepareAnswers() {
     answers.push({
       text: props.correctAnswer,
@@ -29,7 +35,7 @@ function Question(props) {
     <Card>
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text>{props.question}</Card.Text>
+        <Card.Text>{htmlDecode(props.question)}</Card.Text>
         {answers.length > 0 && answers.map((element, index) => {
           return (
             <Button key={index} variant="secondary" size="large" className="m-2" onClick={() => props.onAnswerClicked(element.correct)}>
